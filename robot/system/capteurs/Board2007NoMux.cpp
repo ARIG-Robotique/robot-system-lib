2a42598555ddafff2002e71a259b709610c2736b
Gregory DEPUILLE <gregory.depuille@gmail.com>
2013-04-14 00:34:50 +0200

    Gestion capteur

--- robot/system/capteurs/Board2007NoMux.cpp
+++ robot/system/capteurs/Board2007NoMux.cpp
@@ -8,6 +8,27 @@
 #include "Board2007NoMux.h"
 
 Board2007NoMux::Board2007NoMux() {
-	// TODO Auto-generated constructor stub
+}
 
+void Board2007NoMux::setPinForCapteur(char capteurId, char pin) {
+	if (check(capteurId)) {
+		pinMode(pin, INPUT);
+		this->capteurPins[capteurId] = pin;
+	}
+}
+
+char Board2007NoMux::readCapteurValue(char capteurId) {
+	if (check(capteurId)) {
+		return digitalRead(this->capteurPins[capteurId]);
+	}
+
+	return -1;
+}
+
+boolean Board2007NoMux::check(char capteurId) {
+	if (capteurId >= 0 && capteurId <= 24) {
+		return true;
+	}
+
+	return false;
 }
