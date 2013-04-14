f4b63dfc51c2737afc798a5542ca218435c789a0
Gregory DEPUILLE <gregory.depuille@gmail.com>
2013-04-14 23:58:00 +0200

    Classe utilitaire pour l'I2C

---
+++ robot/utils/I2CUtils.h
@@ -0,0 +1,30 @@
+/*
+ * I2CUtils.h
+ *
+ *  Created on: 14 avr. 2013
+ *      Author: mythril
+ */
+
+#ifndef I2CUTILS_H_
+#define I2CUTILS_H_
+
+#include <Arduino.h>
+
+#define I2C_ACK					0
+#define I2C_DATA_TOO_LONG		1
+#define I2C_NACK_BAD_ADDRESS	2
+#define I2C_NACK_BAD_DATA		3
+#define I2C_OTHER_ERROR			4
+
+class I2CUtils {
+public:
+	I2CUtils();
+
+	void printReturnCode(byte code);
+	boolean isError(byte code);
+	boolean isOk(byte code);
+};
+
+extern I2CUtils i2cUtils;
+
+#endif /* I2CUTILS_H_ */
