23a8afe2f91bd01417c747d1f71751d1d95bd6ec
Gregory DEPUILLE <gregory.depuille@gmail.com>
2013-05-04 14:17:59 +0200

    Ajout de commentaire

--- filters/Pid.cpp
+++ filters/Pid.cpp
@@ -7,6 +7,11 @@
 
 #include "Pid.h"
 
+#include <Arduino.h>
+
+/*
+ * Constructeur de la classe de gestion du PID
+ */
 Pid::Pid() {
 	kp = 0.1;
 	kd = 0.5;
@@ -16,23 +21,32 @@ Pid::Pid() {
 	lastError = 0;
 }
 
+/*
+ * Réinitialise les variables du PID
+ */
 void Pid::reset() {
 	errorSum = 0;
 	lastError = 0;
 }
 
+/*
+ * Configuration des coëfficient du PID
+ */
 void Pid::setTunings(double kp, double ki, double kd) {
 	this->kp = kp;
 	this->ki = ki;
 	this->kd = kd;
 }
 
+/*
+ * Calcul du PID en fonction de la consigne et de la mesure
+ */
 double Pid::compute(double consigne, double mesure) {
 	double error = consigne - mesure;
 	double deltaError = error - lastError;
-	lastError = error;
 	errorSum += error;
-
 	double result = kp * error + ki * errorSum + kd * deltaError;
+
+	lastError = error;
 	return result;
 }
