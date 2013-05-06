c849efcbcd934c3a7ef376bf6147439dc1a1a855
Heiginko <heiginko@gmail.com>
2013-05-06 12:23:27 +0200

    Modification nom du fichier PathFinding

---
+++ robot/system/PathFinding.h
@@ -0,0 +1,18 @@
+/*
+ * PathFinding.h
+ *
+ *  Created on: 1 janv. 2013
+ *      Author: mythril
+ */
+
+#ifndef PATHFINDING_H_
+#define PATHFINDING_H_
+
+// TODO : Implémenter l'algorithme
+
+class PathFinding {
+public:
+	PathFinding();
+};
+
+#endif /* PATHFINDING_H_ */
