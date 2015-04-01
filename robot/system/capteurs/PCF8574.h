d57c7c5fef35603ca259a6e9a3edbe79f566d5a7
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2015-04-01 19:16:59 +0200

    Formatage et ajustement de la lib PCF8574
    Signed-off-by: Gregory DEPUILLE <gregory.depuille@sogelink.fr>

--- robot/system/capteurs/PCF8574.h
+++ robot/system/capteurs/PCF8574.h
@@ -13,36 +13,31 @@
 #ifndef _PCF8574_H
 #define _PCF8574_H
 
-#if defined(ARDUINO) && ARDUINO >= 100
-#include "Arduino.h"
-#else
-#include "WProgram.h"
-#endif
+#include <Arduino.h>
 
 #define PCF8574_LIB_VERSION "0.1.02"
 
-class PCF8574
-{
-  public:
-  PCF8574(int address);
+class PCF8574 {
+public:
+	PCF8574(int address);
 
-  uint8_t read8();
-  uint8_t read(uint8_t pin);
-  uint8_t value();
+	uint8_t read8();
+	uint8_t read(uint8_t pin);
+	uint8_t value();
 
-  void write8(uint8_t value);
-  void write(uint8_t pin, uint8_t value);
+	void write8(uint8_t value);
+	void write(uint8_t pin, uint8_t value);
 
-  void toggle(uint8_t pin);
-  void shiftRight(uint8_t n=1);
-  void shiftLeft(uint8_t n=1);
+	void toggle(uint8_t pin);
+	void shiftRight(uint8_t n = 1);
+	void shiftLeft(uint8_t n = 1);
 
-  int lastError();
+	int lastError();
 
-  private:
-  int _address;
-  uint8_t _data;
-  int _error;
+private:
+	int _address;
+	uint8_t _data;
+	int _error;
 };
 
 #endif
