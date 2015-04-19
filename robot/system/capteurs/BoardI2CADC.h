4febd56436a50b79d86cc692f2fb14609c080d4e
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2015-04-20 01:06:02 +0200

    Réorganisation
    Signed-off-by: Gregory DEPUILLE <gregory.depuille@sogelink.fr>

--- robot/system/capteurs/BoardI2CADC.h
+++ robot/system/capteurs/BoardI2CADC.h
@@ -12,18 +12,16 @@
 #include <Wire.h>
 #include "../../../common.h"
 
-#define I2C_ADC_IO_COUNT 		8
-
-#define DIFFERENTIAL_INPUTS 	0
-#define SINGLE_ENDED_INPUTS 	1
-
-#define POWER_DOWN_BETWEEN_AD 	0
-#define INTERNAL_REF_OFF_AD_ON 	1
-#define INTERNAL_REF_ON_AD_OFF	2
-#define INTERNAL_REF_ON_AD_ON	3
-
 class BoardI2CADC {
 public:
+	#define DIFFERENTIAL_INPUTS 	0
+	#define SINGLE_ENDED_INPUTS 	1
+
+	#define POWER_DOWN_BETWEEN_AD 	0
+	#define INTERNAL_REF_OFF_AD_ON 	1
+	#define INTERNAL_REF_ON_AD_OFF	2
+	#define INTERNAL_REF_ON_AD_ON	3
+
 	BoardI2CADC(int address);
 
 	void inputMode(byte value);
@@ -31,6 +29,8 @@ public:
 
 	int readCapteurValue(byte capteurId);
 private:
+	#define I2C_ADC_IO_COUNT 		8
+
 	// Adresse des canaux ADC
 	const byte CH_ADD[I2C_ADC_IO_COUNT] = {0, 4, 1, 5, 2, 6, 3, 7};
 	int address;
